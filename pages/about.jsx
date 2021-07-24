7c8627276f4814d5ce928c471ea80b589775a754
Ahmad Fakhrudin A <ahmad_fakhrudin@outlook.com>
2021-07-24 23:26:42 +0700

    added pics

--- pages/about.jsx
+++ pages/about.jsx
@@ -13,7 +13,7 @@ function about() {
             <Content>
                <div>
                     <h2>Tentang Kelompok Pengrajin Besek di Desa Kalinanas</h2>
-                    <p>Desa Kalinanas terletak di Kecamatan Wonosamodro, Kabupaten Boyolali, Provinsi Jawa Tengah. Di Desa ini terdapat kelompok-kelompok masyarakat yang memproduksi anyaman besek ukuran sedang. Besek-besek tersebut biasa dijual ke Jepara untuk wadah ikan. Hasil dari penjualan di ... . Satu kelompok pengrajin besek mampu menghasilkan 2-4 ikat (125 pcs/ikat) setiap pertemuan atau kegiatan. Pertemuan biasa dilakukan seminggu sekali di hari minggu. Harga satu ikat besek dipatok di kisaran 10 ribu rupiah.</p>
+                    <p>Desa Kalinanas terletak di Kecamatan Wonosamodro, Kabupaten Boyolali, Provinsi Jawa Tengah. Di Desa ini terdapat kelompok-kelompok masyarakat yang memproduksi anyaman besek ukuran sedang. Besek-besek tersebut biasa dijual ke Jepara untuk wadah ikan. Laba dari penjualan besek dibagikan ke tiap anggota. Satu kelompok pengrajin besek mampu menghasilkan 2-4 ikat (125 pcs/ikat) setiap pertemuan atau kegiatan. Pertemuan biasa dilakukan seminggu sekali di hari minggu. Harga satu ikat besek dipatok di kisaran 10 ribu rupiah.</p>
                </div>
                <div>
                     <p>Kolom masukan</p>
--- pages/gallery.jsx
+++ pages/gallery.jsx
@@ -23,28 +23,55 @@ function gallery() {
                 <div >
                     <ul className={styles.gallery}>
                         <li>
-                            <Image src='/front-1.jpeg' alt='' layout='responsive' width={1280} height={720}/>
+                            <Image src='/img/1.jpeg' alt='' layout='responsive' width={1280} height={720}/>
                         </li>
                         <li>
-                            <Image src='/front-1.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                            <Image src='/img/2.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
                         </li>
                         <li>
-                            <Image src='/front-1.jpeg' alt='' layout='responsive' width={1280} height={720}/>
+                            <Image src='/img/3.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
                         </li>
                         <li>
-                            <Image src='/front-1.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                            <Image src='/img/4.jpeg' alt='' layout='responsive' width={1280} height={720}/>
                         </li>
                         <li>
-                            <Image src='/front-1.jpeg' alt='' layout='responsive' width={1280} height={720}/>
+                            <Image src='/img/5.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
                         </li>
                         <li>
-                            <Image src='/front-1.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                            <Image src='/img/6.jpeg' alt='' layout='responsive' width={1280} height={720}/>
                         </li>
                         <li>
-                            <Image src='/front-1.jpeg' alt='' layout='responsive' width={1280} height={720}/>
+                            <Image src='/img/7.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
                         </li>
                         <li>
-                            <Image src='/front-1.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                            <Image src='/img/8.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                        </li>
+                        <li>
+                            <Image src='/img/9.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                        </li>
+                        <li>
+                            <Image src='/img/10.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                        </li>
+                        <li>
+                            <Image src='/img/11.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                        </li>
+                        <li>
+                            <Image src='/img/12.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                        </li>
+                        <li>
+                            <Image src='/img/13.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                        </li>
+                        <li>
+                            <Image src='/img/14.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                        </li>
+                        <li>
+                            <Image src='/img/15.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                        </li>
+                        <li>
+                            <Image src='/img/16.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
+                        </li>
+                        <li>
+                            <Image src='/img/17.jpeg' alt='' layout='responsive' width={1280} height={720}/>   
                         </li>
                     </ul>
                 </div>
